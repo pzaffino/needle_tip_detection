4d9fa89dd7ab1484a8dc75d7143317e7aa4c8b0f
Paolo Zaffino <p.zaffino@unicz.it>
2016-07-12 14:23:24 +0200

    Pure python script should be ready. It has also comments inside.

--- needle_CNN_LASAGNE_PATCH.py
+++ needle_CNN_LASAGNE_PATCH.py
@@ -18,9 +18,27 @@ from skimage.morphology import binary_closing
 
 import sklearn.cross_validation
 
+def iterate_minibatches(inputs, targets, batchsize, shuffle=True):
+    """
+    This function creates chunk of data for CNN training.
+    """
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+
 
 def extract_patches(mri, label, half_patch_size=5, negative_subsample_ratio=500):
-    
+    """
+    This function divide the MRI into a serie of patches.
+    Each patch is classified as "positive" or "negative" on basis of the label.
+    """
     assert mri.shape == label.shape
     
     z_size, x_size, y_size = mri.shape
@@ -48,7 +66,9 @@ def extract_patches(mri, label, half_patch_size=5, negative_subsample_ratio=500)
 
 
 def patchimg2differentview(patch):
-    
+    """
+    This function creates a sort of 3D patch by building a patch containing the volume depicted in 3 views 
+    """
     z_size, x_size, y_size = patch.shape
     
     X = np.zeros((z_size * 3, x_size, y_size), dtype=np.float32)
@@ -68,7 +88,10 @@ def patchimg2differentview(patch):
 
 
 def patches2CNNformat(patches, label, half_patch_size=5):
-    
+    """
+    This function takes a list of patches and create a 4D matrix.
+    The patch is represented as a sort of 3D view.
+    """ 
     X = np.zeros((len(patches), 3 * ((2 * half_patch_size)+1), (half_patch_size*2)+1, (half_patch_size*2)+1), dtype=np.float32)
     y = np.zeros((len(patches)), dtype=np.int32) * -1
     
@@ -82,6 +105,10 @@ def patches2CNNformat(patches, label, half_patch_size=5):
 
 
 def get_resized_img(img, data_type = sitk.sitkFloat32):
+    """
+    This function resizes an image to a fixed shape.
+    If data type is sitkFloat32 a linear interpolation is used, otherwise nearest neighbor interpolation is used.
+    """
     
     size = img.GetSize()
     ratio = [1.0/i for i in img.GetSpacing()]
@@ -101,28 +128,11 @@ def get_resized_img(img, data_type = sitk.sitkFloat32):
     return sitk.GetArrayFromImage(new_image)
 
 
-def needles2tips(only_needles, image_array, number_of_slices=3):
-    needles_masks_array = np.zeros_like(image_array).astype(float)  
-    for file_item in only_needles:
-        this_mask = file_item.astype(np.float)
-        if np.sum(this_mask) < (np.shape(this_mask)[0] * np.shape(this_mask)[1] * np.shape(this_mask)[2]):
-            this_mask = binary_closing(this_mask,selem=np.ones((3,3,3)))
-            found=False
-            row = np.shape(this_mask)[0]-1
-            while found==False and row > 0: #< np.shape(this_mask)[0]-1 :
-                #print(row)
-                this_row = this_mask[row,:,:]
-                if np.sum(this_row) > 0:
-                    #print(row)
-                    found = True
-                    temp = np.add(needles_masks_array[row:row+1+number_of_slices,:,:],this_mask[row:row+1+number_of_slices,:,:])
-                    temp[temp!=0] = 1
-                    needles_masks_array[row:row+1+number_of_slices,:,:] = temp 
-                row -= 1
-    return needles_masks_array.astype(np.int32)
-
-
-def needles2tips_PAOLO(needles, mri, number_of_slices=3):
+def needles2tips(needles, mri, number_of_slices=3):
+    """
+    This function accepts a list of volumes (each volumes containing a needle) and returns a single volume containing just the tips.
+    Here there are a plenty of attempts of removing untrusted data.
+    """
     tips = np.zeros_like(mri).astype(np.int32)
     #print(tips.shape)
     for needle in needles:
@@ -148,11 +158,43 @@ def needles2tips_PAOLO(needles, mri, number_of_slices=3):
 
 
 def pad_volume(img, half_patch_size=5):
+    """
+    This function pads a volume in order to extract the patches without losing the information at the border
+    """
     npad = ((half_patch_size,half_patch_size),(half_patch_size,half_patch_size),(half_patch_size,half_patch_size))
     return np.lib.pad(img, npad, "constant", constant_values=0)
 
 
+def join_Xy_posneg(X_pos, y_pos, X_neg, y_neg, test_size1=0.20, test_size2=0.33):
+    """
+    This function takes X_pos, y_pos, X_neg, y_neg and return X_train, X_val, X_test, y_train, y_val, y_test.
+    Test size is first equal to test_size1% of X, then it is split and val size is taken away from it. val size is test_size2% of test size.
+    """
+    
+    X_pos_train, X_pos_test, y_pos_train, y_pos_test = sklearn.cross_validation.train_test_split(X_pos, y_pos, test_size=test_size1)
+    X_pos_test, X_pos_val, y_pos_test, y_pos_val = sklearn.cross_validation.train_test_split(X_pos_test, y_pos_test, test_size=test_size2)
+
+    X_neg_train, X_neg_test, y_neg_train, y_neg_test = sklearn.cross_validation.train_test_split(X_neg, y_neg, test_size=test_size1)
+    X_neg_test, X_neg_val, y_neg_test, y_neg_val = sklearn.cross_validation.train_test_split(X_neg_test, y_neg_test, test_size=test_size2)
+
+    X_train = np.concatenate((X_neg_train, X_pos_train), axis=0).astype(np.float32)
+    y_train = np.concatenate((y_neg_train, y_pos_train), axis=0).astype(np.int32)
+
+    X_val = np.concatenate((X_neg_val, X_pos_val), axis=0).astype(np.float32)
+    y_val = np.concatenate((y_neg_val, y_pos_val), axis=0).astype(np.int32)
+
+    X_test = np.concatenate((X_neg_test, X_pos_test), axis=0).astype(np.float32)
+    y_test = np.concatenate((y_neg_test, y_pos_test), axis=0).astype(np.int32)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
 def data_for_CNN(general_folder, half_patch_size=5):
+    """
+    This is the main function for data generation. It accepts the root folder and returns X_pos, y_pos, X_neg, y_neg.
+    Also here there is some strange stuff for trying to exclude untrusted data.
+    """
+    
     folders_cases = os.listdir(general_folder)
     
     X_pos = np.ones((1, 3 * ((2 * half_patch_size)+1), (half_patch_size*2)+1, (half_patch_size*2)+1), dtype=np.float32) * -1.0
@@ -192,7 +234,7 @@ def data_for_CNN(general_folder, half_patch_size=5):
                 
         print(" %d valid needles" %  (valid_needles))
         
-        tips = needles2tips_PAOLO(needles, sitk.GetArrayFromImage(mri_sitk))
+        tips = needles2tips(needles, sitk.GetArrayFromImage(mri_sitk))
         tips_sitk = sitk.GetImageFromArray(tips)
         tips_sitk.CopyInformation(mri_sitk)
         tips = get_resized_img(tips_sitk, sitk.sitkInt16)
@@ -240,29 +282,11 @@ def data_for_CNN(general_folder, half_patch_size=5):
     return X_pos, y_pos, X_neg, y_neg
 
 
-def join_Xy_posneg(X_pos, y_pos, X_neg, y_neg, test_size1=0.20, test_size2=0.33):
-    
-    X_pos_train, X_pos_test, y_pos_train, y_pos_test = sklearn.cross_validation.train_test_split(X_pos, y_pos, test_size=test_size1)
-    X_pos_test, X_pos_val, y_pos_test, y_pos_val = sklearn.cross_validation.train_test_split(X_pos_test, y_pos_test, test_size=test_size2)
 
-    X_neg_train, X_neg_test, y_neg_train, y_neg_test = sklearn.cross_validation.train_test_split(X_neg, y_neg, test_size=test_size1)
-    X_neg_test, X_neg_val, y_neg_test, y_neg_val = sklearn.cross_validation.train_test_split(X_neg_test, y_neg_test, test_size=test_size2)
-
-    X_train = np.concatenate((X_neg_train, X_pos_train), axis=0).astype(np.float32)
-    y_train = np.concatenate((y_neg_train, y_pos_train), axis=0).astype(np.int32)
-
-    X_val = np.concatenate((X_neg_val, X_pos_val), axis=0).astype(np.float32)
-    y_val = np.concatenate((y_neg_val, y_pos_val), axis=0).astype(np.int32)
-
-    X_test = np.concatenate((X_neg_test, X_pos_test), axis=0).astype(np.float32)
-    y_test = np.concatenate((y_neg_test, y_pos_test), axis=0).astype(np.int32)
-    
-    return X_train, X_val, X_test, y_train, y_val, y_test
-
-# Define user parameter for data generation, for network and for real case segmentation
-half_patch_size = 5
+# DEFINE USER PARAMETERS FOR DATA GENERATION, NETWORK AND FOR REAL CASE SEGMENTATION
+half_patch_size = 5 # it will produce a patch having size (11,11,11). It can be view as (1+5+5,1+5+5,1+5+5)
 root_folder_data = "../LabelMaps"
-save_data = True
+save_patches = True
 storage_patches_file = "../patches.npz"
 load_or_generate_data = "generate"
 
@@ -276,21 +300,25 @@ real_case_to_segment = '../LabelMaps/64/case.nrrd'
 output_MRI = '../test_pat.nrrd'
 output_label = '../test_label.nrrd'
 
-# Generate or load patches
+
+# GENERATE OR LOAD PATCHES
 if load_or_generate_data == "generate":
     X_pos, y_pos, X_neg, y_neg = data_for_CNN(root_folder_data, half_patch_size=half_patch_size)
 elif load_or_generate_data == "load"
     saved_data = np.load(storage_patches_file)
     X_pos, y_pos, X_neg, y_neg = saved_data['arr_0'], saved_data['arr_1'], saved_data['arr_2'], saved_data['arr_3']
 
-# Save patches
-if save_data:
+
+# SAVE PATCEHS
+if save_patches:
     np.savez(storage_patches_file, X_pos, y_pos, X_neg, y_neg)
 
-# Generate training, testing and validation X and y
+
+# GENERATE TRAINING, TESTING AND VALIDATION X AND y
 X_train, X_val, X_test, y_train, y_val, y_test = join_Xy_posneg(X_pos, y_pos, X_neg, y_neg, test_size1=0.20, test_size2=0.33)
 
-# Normalize dataset
+
+# NORMALIZE DATASET
 m, s = X_train.mean(), X_train.std()
 
 X_train -= m
@@ -304,20 +332,10 @@ X_test /= s
 
 print(X_train.shape, X_test.shape, X_val.shape)
 
-def iterate_minibatches(inputs, targets, batchsize, shuffle=True):
-    assert len(inputs) == len(targets)
-    if shuffle:
-        indices = np.arange(len(inputs))
-        np.random.shuffle(indices)
-    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
-        if shuffle:
-            excerpt = indices[start_idx:start_idx + batchsize]
-        else:
-            excerpt = slice(start_idx, start_idx + batchsize)
-        yield inputs[excerpt], targets[excerpt]
-
-
 def build_cnn(single_entry_shape, input_var=None):
+    """
+    Build a CNN
+    """
     
     network = lasagne.layers.InputLayer(shape=(None, single_entry_shape[0], single_entry_shape[1], 
                                                single_entry_shape[2]),
@@ -349,7 +367,6 @@ def build_cnn(single_entry_shape, input_var=None):
             nonlinearity=lasagne.nonlinearities.softmax)
 
     return network
-
 
 # Prepare Theano variables for inputs and targets
 single_entry_shape = X_train.shape[1:]
@@ -449,13 +466,12 @@ elif train_or_load_network == "train":
 if save_network:
     np.savez(network_pars_file, *lasagne.layers.get_all_param_values(network))
 
-
 # REAL CASE SEGMENTATION
 test_prediction = lasagne.layers.get_output(network, deterministic=True)
 predict_fn = theano.function([input_var], T.argmax(test_prediction, axis=1))
 
 test_pat = get_resized_img(sitk.ReadImage(real_case_to_segment))
-test_pat = test_pat[140:,15:180,80:230]
+test_pat = test_pat[140:,15:180,80:230] # crop the image in order to speed up the process
 test_pat = pad_volume(test_pat, half_patch_size=half_patch_size)
 
 pat_sitk = sitk.GetImageFromArray(test_pat.astype(np.float32))
